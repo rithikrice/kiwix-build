02a6ff51d63407cf17280d914d2e666efb752cca
Chris Li <chrisshwli@gmail.com>
2017-07-25 14:10:29 -0400

    separate targetinfo

--- kiwix-build.py
+++ kiwix-build.py
@@ -124,89 +124,8 @@ def which(name):
     return output[:-1].decode()
 
 
-class TargetInfo:
-    def __init__(self, build, static, toolchains, hosts=None):
-        self.build = build
-        self.static = static
-        self.toolchains = toolchains
-        self.compatible_hosts = hosts
 
-    def __str__(self):
-        return "{}_{}".format(self.build, 'static' if self.static else 'dyn')
 
-    def get_cross_config(self, host):
-        if self.build == 'native':
-            return {}
-        elif self.build == 'win32':
-            root_paths = {
-                'fedora': '/usr/i686-w64-mingw32/sys-root/mingw',
-                'debian': '/usr/i686-w64-mingw32'
-            }
-            return {
-                'root_path': root_paths[host],
-                'extra_libs': ['-lwinmm', '-lws2_32', '-lshlwapi', '-lrpcrt4', '-lmsvcr90'],
-                'extra_cflags': ['-DWIN32'],
-                'host_machine': {
-                    'system': 'Windows',
-                    'lsystem': 'windows',
-                    'cpu_family': 'x86',
-                    'cpu': 'i686',
-                    'endian': 'little',
-                    'abi': ''
-                }
-            }
-        elif self.build == 'armhf':
-            return {
-                'extra_libs': [],
-                'extra_cflags': [],
-                'host_machine': {
-                    'system': 'linux',
-                    'lsystem': 'linux',
-                    'cpu_family': 'arm',
-                    'cpu': 'armhf',
-                    'endian': 'little',
-                    'abi': ''
-                }
-            }
-
-class AndroidTargetInfo(TargetInfo):
-    __arch_infos = {
-        'arm' : ('arm-linux-androideabi', 'arm', 'armeabi'),
-        'arm64': ('aarch64-linux-android', 'aarch64', 'arm64-v8a'),
-        'mips': ('mipsel-linux-android', 'mipsel', 'mips'),
-        'mips64': ('mips64el-linux-android', 'mips64el', 'mips64'),
-        'x86': ('i686-linux-android', 'i686', 'x86'),
-        'x86_64': ('x86_64-linux-android', 'x86_64', 'x86_64'),
-    }
-
-    def __init__(self, arch):
-        super().__init__('android', True, ['android_ndk', 'android_sdk'],
-                         hosts=['fedora', 'debian'])
-        self.arch = arch
-        self.arch_full, self.cpu, self.abi = self.__arch_infos[arch]
-
-    def __str__(self):
-        return "android"
-
-    def get_cross_config(self, host):
-        return {
-            'extra_libs': [],
-            'extra_cflags': [],
-            'host_machine': {
-                'system': 'Android',
-                'lsystem': 'android',
-                'cpu_family': self.arch,
-                'cpu': self.cpu,
-                'endian': 'little',
-                'abi': self.abi
-            },
-        }
-
-class iOSTargetInfo(TargetInfo):
-    def __init__(self, arch):
-        super().__init__('iOS', True, ['iOS_sdk'],
-                         hosts=['Darwin'])
-        self.arch = arch
 
 
 class BuildEnv:
